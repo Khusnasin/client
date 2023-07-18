b125c2bd6ce5a65bd654a79518e5d58453caac94
Khusnasin <khusnasinsultana@gmail.com>
2023-07-19 01:35:15 +0530

    Commit_9

--- src/Screens/Farmersregistration.js
+++ src/Screens/Farmersregistration.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
 import Success from "../components/Success";
 
 function Farmersregistratration() {
+    const formRef = useRef(null);
     const [Name, setname] = useState('');
     //const[email, setemail] = useState('');
     const [location, setlocation] = useState('');
@@ -12,7 +13,8 @@ function Farmersregistratration() {
     const [phoneNumber, setphoneNumber] = useState();
     const [option, setOption] = useState('');
 
-    const [amountOfNapier, setamountOfNapier] = useState(false);
+    const [areaOfNapier, setareaOfNapier] = useState(false);
+    const [useOfNapier, setuseOfNapier] = useState('');
     const [numberOfCows, setnumberOfCows] = useState(false);
     const [dungProduced_inKg, setdungProduced_inKg] = useState(false);
     const [amountOfMilk_inLitre, setamountOfMilk_inLitre] = useState(false);
@@ -21,6 +23,8 @@ function Farmersregistratration() {
     const [imageurl2, setimageurl2] = useState()
     const [imageurl3, setimageurl3] = useState()
     const [description, setdescription] = useState()
+    const [challenges, setchallenges] = useState()
+    const [interestInTraining, setinterestInTraining] = useState()
 
     const [password, setpassword] = useState('');
     const [cpassword, setcpassword] = useState('');
@@ -29,27 +33,55 @@ function Farmersregistratration() {
     const [error, seterror] = useState();
     const [success, setsuccess] = useState();
 
+    useEffect(() => {
+        if (success) {
+            resetForm();
+        }
+    }, [success]);
+
+    const resetForm = () => {
+        setname('')
+        setlocation('')
+        setphoneNumber('')
+        setareaOfNapier('')
+        setuseOfNapier('')
+        setnumberOfCows('')
+        setdungProduced_inKg('')
+        setamountOfMilk_inLitre('')
+        setimageurl1('')
+        setimageurl2('')
+        setimageurl3('')
+        setdescription('')
+        setchallenges('')
+        setinterestInTraining('')
+
+        setpassword('')
+        setcpassword('')
+
+    };
     async function register() {
+
         if (password === cpassword) {
             const farmer = {
                 Name,
                 location,
                 phoneNumber,
-                amountOfNapier,
-
+                areaOfNapier,
+                useOfNapier,
                 numberOfCows,
                 dungProduced_inKg,
                 amountOfMilk_inLitre,
                 imageUrls: [imageurl1, imageurl2, imageurl3],
                 description,
-
+                challenges,
+                interestInTraining,
                 password,
                 cpassword
             }
 
             try {
                 setloading(true);
-                const response = await axios.post('/api/farmers/registerfarmer', farmer,{
+                const response = await axios.post('/api/farmers/registerfarmer', farmer, {
                     headers: {
                         'Content-Type': 'application/json',
                     },
@@ -61,7 +93,8 @@ function Farmersregistratration() {
                 setname('')
                 setlocation('')
                 setphoneNumber()
-                setamountOfNapier()
+                setareaOfNapier()
+                setuseOfNapier()
                 setnumberOfCows()
                 setdungProduced_inKg()
                 setamountOfMilk_inLitre()
@@ -69,9 +102,13 @@ function Farmersregistratration() {
                 setimageurl2()
                 setimageurl3()
                 setdescription()
-
+                setchallenges()
+                setinterestInTraining()
+                
                 setpassword('')
                 setcpassword('')
+
+                resetForm();
 
             } catch (error) {
                 console.log(error);
@@ -83,26 +120,42 @@ function Farmersregistratration() {
             alert('Please check the password again!')
         }
     }
+    const handleReset = () => {
+        resetForm();
+    }
     const handleOptionChange = (event) => {
         const selectedOption = event.target.value;
         setOption(selectedOption);
 
         // Enable/disable textboxes based on the selected option
         if (selectedOption === 'Napier Grass') {
-            setamountOfNapier(true);
+            setareaOfNapier(true);
+            setuseOfNapier('');
             setnumberOfCows(false);
             setdungProduced_inKg(false);
             setamountOfMilk_inLitre(false);
         } else if (selectedOption === 'Cows') {
-            setamountOfNapier(false);
+            setareaOfNapier(false);
+            setuseOfNapier('');
             setnumberOfCows(true);
             setdungProduced_inKg(true);
             setamountOfMilk_inLitre(true);
         } else if (selectedOption === 'both') {
-            setamountOfNapier(true);
+            setareaOfNapier(true);
+            setuseOfNapier('');
             setnumberOfCows(true);
             setdungProduced_inKg(true);
             setamountOfMilk_inLitre(true);
+        }
+    };
+    const handleRadioChange = async (event) => {
+        const value = event.target.value === 'yes'; // Convert to boolean
+
+        try {
+
+            setinterestInTraining(value);
+        } catch (error) {
+            console.error('Error updating user:', error);
         }
     };
 
@@ -117,7 +170,7 @@ function Farmersregistratration() {
                 {error && (<Error />)}
                 <div className="bs">
 
-                    <div className="col-md-10" style={{ marginTop: '80px', marginLeft: '60px' }}>
+                    <div className="col-md-8" style={{ marginRight: '20px', marginTop: '80px' }}>
 
 
 
@@ -139,11 +192,38 @@ function Farmersregistratration() {
                         <input type='text' className='form-control' placeholder='description'
                             value={description} onChange={(e) => { setdescription(e.target.value) }}
                         />
+                        <input type='text' className='form-control' placeholder='Any specific challenges you face in your farming practices:'
+                            value={challenges} onChange={(e) => { setchallenges(e.target.value) }}
+                        />
+
+                        <div className="radio-container">
+                            <label style={{ marginTop: '15px', marginLeft: '-100px', fontSize: '18px' }}>Are you interested in any farm-related training or <br></br>assistance?</label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="yes"
+                                    checked={interestInTraining}
+                                    onChange={handleRadioChange}
+                                />
+                                Yes
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="no"
+                                    checked={!interestInTraining}
+                                    onChange={handleRadioChange}
+                                />
+                                No
+                            </label>
+                        </div>
                     </div>
 
 
-                    <div className="col-md-10" >
+                    <div className="col-md-8" >
+
                         <div className="radio-container">
+                            <label style={{ marginTop: '15px', marginLeft: '-100px', fontSize: '18px' }}>Select your preference: </label>
                             <label>
                                 <input
                                     type="radio"
@@ -181,8 +261,10 @@ function Farmersregistratration() {
 
 
                         <br />
-                        <input type="number" disabled={option !== 'Napier Grass' && option !== 'both'} className="form-control" placeholder="Amount of Napier grass(in kg)"
-                            value={amountOfNapier} onChange={(e) => { setamountOfNapier(e.target.value) }} />
+                        <input type="number" disabled={option !== 'Napier Grass' && option !== 'both'} className="form-control" placeholder="Total Area of Napier Grass Cultivation (in acres/hectares)"
+                            value={areaOfNapier} onChange={(e) => { setareaOfNapier(e.target.value) }} />
+                        <input type="text" disabled={option !== 'Napier Grass' && option !== 'both'} className="form-control" placeholder="How do you use Napier grass on your farm? (e.g., feed for cows, selling, other purposes)"
+                            value={useOfNapier} onChange={(e) => { setuseOfNapier(e.target.value) }} />
                         <input type="number" disabled={option !== 'Cows' && option !== 'both'} className="form-control" placeholder="Number Of Cows"
                             value={numberOfCows === true ? '' : numberOfCows} onChange={(e) => { setnumberOfCows(e.target.value) }} />
                         <input type="number" disabled={option !== 'Cows' && option !== 'both'} className="form-control" placeholder="Dung Produced (in kg)"
@@ -198,7 +280,8 @@ function Farmersregistratration() {
                     <div className='text-center'>
 
 
-                        <button className="btn btn-primary mt-3" style={{ marginLeft: '90%' }} onClick={register}>Register</button>
+                        <button className="btn btn-primary mt-3" style={{ marginRight: '10px' }} onClick={register}>Register</button>
+                        <button className="btn btn-primary mt-3" type="button" onClick={handleReset}>Reset</button>
                     </div>
                 </div>
 
