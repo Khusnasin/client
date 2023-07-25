6d88bfaf97063313e1cfa0c4c7045473f23d6e6e
Abhipsha Neog <abhipshaneog0381@gmail.com>
2023-07-26 04:28:49 +0530

    modified:   src/Screens/Adminlogin.js 	modified:   src/Screens/Adminscreen.js

--- src/Screens/Adminlogin.js
+++ src/Screens/Adminlogin.js
@@ -1,4 +1,92 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Adminlogin() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [adminCode, setadminCode] = useState('');
+
+  const handleLogin = async () => {
+    // Make the login request to the server and get the 'isAdmin' value from the response
+    try {
+      const response = await axios.post('/api/admin/login', {
+        email,
+        password,
+      });
+
+      const { isAdmin } = response.data; // Assuming the server returns isAdmin
+
+      // Set the 'currentUser' object in localStorage
+      localStorage.setItem(
+        'currentUser',
+        JSON.stringify({
+          email,
+          adminCode,
+          isAdmin,
+          // Other user properties...
+        })
+      );
+      const currentUser = {
+        email: 'admin@example.com',
+        isAdmin: true,
+        // Other user properties...
+      };
+
+      // Set the 'isAdmin' state to true so the component can handle the redirect
+      setIsAdmin(isAdmin);
+
+      // Handle successful login, e.g., redirect to AdminScreen or perform other actions
+      window.location.href = '/admin'; // Redirect to the AdminScreen after successful login
+    } catch (error) {
+      // Handle login error, e.g., show error message
+      console.error('Login error:', error);
+    }
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin();
+  };
+
+  return (
+    <div>
+      <h1>Admin Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+                    type="password"
+                    className="form-control"
+                    placeholder="admincode"
+                    name="adminCode"
+                    value={adminCode}
+                    onChange={(e) => setadminCode(e.target.value)}
+                    required
+                  />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Adminlogin;
+
+/*import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Loader from "../components/Loader";
@@ -29,7 +117,7 @@ function LoginAdmin() {
         if (response.status === 200) {
           setLoading(false);
           localStorage.setItem("token", response.data.token); // Save the token in local storage
-          navigate("/admin"); // Redirect to the admin-dashboard after successful login
+          navigate("/admin"); // Redirect to the admin-screen after successful login
         }
       } catch (error) {
         setLoading(false);
@@ -96,4 +184,4 @@ function LoginAdmin() {
     );
   }
   
-  export default LoginAdmin;
+  export default LoginAdmin;*/
