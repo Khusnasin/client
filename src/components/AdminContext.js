7aab7539a399e18c6dd1a1a67390707e2b207fa7
Abhipsha Neog <abhipshaneog0381@gmail.com>
2023-07-27 01:50:40 +0530

    new file:   src/components/AdminContext.js

---
+++ src/components/AdminContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState} from 'react';
+
+
+// Create the AdminContext
+export const AdminContext= createContext();
+
+
+// Create the AdminProvider component to wrap the application
+ export const AdminProvider = ({ children }) => {
+    const [farmersData, setFarmersData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+
+
+  return (
+    <AdminContext.Provider value={{ farmersData, setFarmersData, loading, setLoading, error, setError, isAdmin, setIsAdmin }}>
+      {children}
+    </AdminContext.Provider>
+  );
+};
+
+
+ 
