5863d431a67473eea93d0ef397cd106d46881b92
Nikita <nikita536@gmail.com>
2023-07-27 12:33:55 +0530

    comit but

---
+++ src/Screens/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import landingscreen from './landingscreen';
+
+function App() {
+  return (
+    <div className="App">
+      <landingscreen />
+    </div>
+  );
+}
+
+export default App;
