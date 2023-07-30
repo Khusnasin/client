5d32de547949cce9dea122b9cf15181b237cf9ad
Khusnasin <khusnasinsultana@gmail.com>
2023-07-30 17:40:47 +0530

    Commit-19

--- src/Screens/landingscreen.js
+++ src/Screens/landingscreen.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import './landing.css'; 
+import { Link } from "react-router-dom";
+
 
 const LandingScreen = () => {
   return (
-    <div className="landing-container">
-      <div className="buttons-container">
+    <div className="row landing justify-content-center">
+      <div className="col-md-9 my-auto text-center" style={{ borderRight: '8px solid white' }}>
+        <h2 style={{ color: 'white', fontSize: '110px' }}>Bluecore</h2>
+        <h3 style={{ color: 'white', fontSize: '40px' }}>"A collection of farmers data"</h3>
+    
+        <Link to='/registerfarmer'>
         <div className="farmers-buttons">
-          <button className="Farmerregistration">Farmers Register</button>
-          <button className="Farmerlogin">Farmers Login</button>
+            <button className="btn landingbtn">Farmers Register</button>
+          </div>
+        </Link>
+
+        <Link to='/loginfarmer'>
+          <div className="farmers-buttons">
+            <button className="btn landingbtn">Farmers Login</button>
         </div>
+        </Link>
+
+        <div className="separator-line"></div>
+
+        <Link to='/registeruseradmin'>
         <div className="admins-buttons">
-        <button className="Adminregistration">Admin Register</button>
-          <button className="Adminregistration">Admin Login</button>
+            <button className="btn landingbtn">Admin Register</button>
+          </div>
+        </Link>
+
+        <Link to='/loginadmin'>
+          <div className="admins-buttons">
+            <button className="btn landingbtn">Admin Login</button>
         </div>
+        </Link>
       </div>
-      <div className="separator-line"></div>
+
+      <Link to='/home'>
+        <button className="btn landingbtn" style={{ color: 'black' }}>Get Started</button>
+      </Link>
+
     </div>
   );
 };
