0e3b2d4792371279f34cd674a89d335e79775f51
Abhipsha Neog <abhipshaneog0381@gmail.com>
2023-08-02 17:13:59 +0530

    modified:   src/Screens/Farmersdashboard.js 	modified:   src/components/Farmersprofile.js

--- src/components/Farmersprofile.js
+++ src/components/Farmersprofile.js
@@ -4,15 +4,15 @@ import axios from "axios";
 //import mongoose from "mongoose";
 
 
-import { Types } from "mongoose";
+//import { Types } from "mongoose";
 import Loader from "./Loader";
 import Error from "./Error";
 
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
 function Farmersprofile() {
   const farmer = JSON.parse(localStorage.getItem("currentUser"))
-  const [farmer1, setFarmer] = useState(farmer.data);
+  const [farmerData, setFarmerData] = useState(null);
   const [editMode, setEditMode] = useState(false);
   const [loading, setloading] = useState(false);
   const [error, seterror] = useState();
@@ -28,13 +28,13 @@ function Farmersprofile() {
     if (!localStorage.getItem('currentUser')) {
       window.location.href = '/login';
     }
-    const farmer = JSON.parse(localStorage.getItem("currentUser"))
+    //const farmer = JSON.parse(localStorage.getItem("currentUser"))
     try {
       setloading(true);
-      console.log("farmerid:", farmerid);
+      //console.log("farmerid:", farmerid);
       //const objectIdFarmerId = Types.ObjectId(farmerid);
       const response = await axios.get(`/api/farmers/getfarmerbyid?farmerid=${farmerid}`);
-      setFarmer(response.data);
+      setFarmerData(response.data);
       setloading(false);
     } catch (error) {
       console.log("Error fetching farmer details:", error);
@@ -47,17 +47,13 @@ function Farmersprofile() {
     fetchFarmerDetails();
   }, [farmerid]);
 
-  useEffect(() => {
-    console.log("farmer state:", farmer);
-  }, [farmer]);
-
   const handleEditButtonClick = async () => {
     if (editMode) {
       // If in edit mode, cancel and reset to server data
       try {
         setloading(true);
         const response = await axios.get(`/api/farmers/getfarmerbyid?farmerid=${farmerid}`);
-        setFarmer(response.data);
+        setFarmerData(response.data);
         setloading(false);
         setEditMode(false);
       } catch (error) {
@@ -72,48 +68,52 @@ function Farmersprofile() {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFarmer((prevFarmer) => ({
-      ...prevFarmer,
+    setFarmerData((prevData) => ({
+      ...prevData,
       [name]: value,
     }));
   };
 
-  console.log(farmer);
+  //console.log(farmer);
 
   const handleRadioChange = async (event) => {
     const value = event.target.value === 'yes';
-
     try {
 
-      setFarmer((prevFarmer) => ({
-        ...prevFarmer,
+      setFarmerData((prevData) => ({
+        ...prevData,
         interestInTraining: value,
       }));
     } catch (error) {
       console.error('Error updating user:', error);
     }
   };
-  const [editedFarmer, setEditedFarmer] = useState();
+  
+  const handleOptionSelect = (selectedOption) => {
+    setFarmerData((prevData) => ({
+      ...prevData,
+      location: selectedOption,
+      setIsDropdownOpen: true
+      
+    }));
+  };
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  //const [editedFarmer, setEditedFarmer] = useState();
   const handleSaveChanges = async () => {
     try {
-      const editedData = {
-        Name: editedFarmer.Name,
-        location: editedFarmer.location,
-        phoneNumber: editedFarmer.phoneNumber,
-        password: editedFarmer.password,
-        areaOfNapier: editedFarmer.areaOfNapier,
-        useOfNapier: editedFarmer.useOfNapier,
-        numberOfCows: editedFarmer.numberOfCows,
-        dungProduced_inKg: editedFarmer.dungProduced_inKg,
-        amountOfMilk_inLitre: editedFarmer.amountOfMilk_inLitre,
-        description: editedFarmer.description,
-        challenges: editedFarmer.challenges,
-        interestInTraining: editedFarmer.interestInTraining
-      };
-      await axios.put(`/api/farmers/updatefarmer/${farmerid}`, editedData);
-      console.log(editedData)
+      await axios.put(`/api/farmers/updatefarmer/${farmerid}`, farmerData);
       setEditMode(false);
-      setFarmer(editedData);
       alert("Changes saved successfully!");
     } catch (error) {
       console.log("Error updating farmer details:", error);
@@ -121,45 +121,12 @@ function Farmersprofile() {
     }
   };
 
-  const handleInputChange2 = (e) => {
-    const { location, value } = e.target
-    setFarmer((prevFarmer) => ({
-      ...prevFarmer,
-      [location]: value,
-      setIsDropdownOpen: true
-    }));
-  };
-
-  const handleOptionSelect = (selectedOption) => {
-    setFarmer((prevFarmer) => ({
-      ...prevFarmer,
-      location: selectedOption,
-      setIsDropdownOpen: false
-    }));
-  };
-  console.log(farmer)
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   return (
     <div >
       <h2>{farmer.data.Name}'s Profile</h2>
-      {loading ? (<Loader />) : (farmer ? (<div>
+      {loading ? (<Loader />) : (farmerData ? (<div>
         <div className="row bs2">
           <div className="col-md-3">
-
-
-
           </div>
           <div className="col-md-5">
             <h1>My Details</h1>
@@ -167,74 +134,144 @@ function Farmersprofile() {
             <b>
               <h1>Personal Info</h1>
               <hr className="line"></hr>
-              <p>Name : {editMode ? (
-                <input type="text" className="form-control" placeholder="Phone Number"
-                  value={farmer.phoneNumber} onChange={handleInputChange} />) : (
-                <span>{JSON.parse(localStorage.getItem('currentUser')).data.Name}  </span>
-              )}</p>
-              <p>Location : {editMode ? (
-                <div>
-                  <input type="text" className="form-control" placeholder="location"
-                    value={farmer.location} onChange={handleInputChange2} onClick={() => setIsDropdownOpen(true)} />
-                  {isDropdownOpen && (
-                    <ul ref={dropdownRef}>
-                      {dropdownOptions.map((option) => (
-                        <li key={option} onClick={() => handleOptionSelect(option)}>
-                          {option}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              ) : (
-                <span>{farmer.data.location}</span>
-              )} </p>
-              <p>Phone-Number : {editMode ? (
-                <input type="text" className="form-control" placeholder="Phone Number"
-                  value={farmer.phoneNumber} onChange={handleInputChange} />) : (
-                <span>{farmer.data.phoneNumber} </span>
-              )}</p>
-              <p>Password : {editMode ? (
-                <input type="password" className="form-control" placeholder="password"
-                  value={farmer.password} onChange={handleInputChange} />) : (
-                <span>{farmer1.password} </span>
+              <p>Name : {" "}
+              {editMode ? (
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    name="Name"
+                    value={farmerData.Name}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <span>{farmerData.Name}</span>
+                )}</p>
+              <p>Location :{" "}
+               {editMode ?  (
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="location"
+                      name="location"
+                      value={farmerData.location}
+                      onChange={handleInputChange}
+                      onClick={() => setIsDropdownOpen(true)}
+                    />
+                    {isDropdownOpen && (
+                      <ul ref={dropdownRef}>
+                        {dropdownOptions.map((option) => (
+                          <li key={option} onClick={() => handleOptionSelect(option)}>
+                            {option}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                ) : (
+                  <span>{farmerData.location}</span>
+                )}
+                 </p>
+              <p>Phone-Number : {" "}
+                {editMode ? (
+                <input
+                type="text"
+                className="form-control"
+                placeholder="Phone Number"
+                name="phoneNumber"
+                value={farmerData.phoneNumber}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.phoneNumber}</span>
+            )}</p>
+              <p>Password :{" "} {editMode ? (
+                 <input
+                 type="password"
+                 className="form-control"
+                 placeholder="password"
+                 name="password"
+                 value={farmerData.password}
+                 onChange={handleInputChange}
+               />
+             ) : (
+               <span>{farmerData.password}</span>
               )}</p>
               <h1>Napier Info</h1>
               <hr className="line"></hr>
-              <p>Area Of Napier : {editMode ? (
-                <input type="number" className="form-control" placeholder="Total Area of Napier Grass Cultivation (in acres/hectares/bigha)"
-                  value={farmer.areaOfNapier} onChange={handleInputChange} />) : (
-                <span>{farmer1.areaOfNapier}</span>
+              <p>Area Of Napier : {" "}
+                {editMode ? (
+                <input
+                type="number"
+                className="form-control"
+                placeholder="Total Area of Napier Grass Cultivation (in acres/hectares/bigha)"
+                name="areaOfNapier"
+                value={farmerData.areaOfNapier}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.areaOfNapier}</span>
               )
               }</p>
             </b>
             <b>
-              <p>Use Of Napier : {editMode ? (
-                <input type="text" className="form-control" placeholder="How do you use Napier grass on your farm? (e.g., feed for cows, selling, other purposes)"
-                  value={farmer.useOfNapier} onChange={handleInputChange} />) : (
-                <span>{farmer1.useOfNapier}</span>
+              <p>Use Of Napier : {" "}
+                {editMode ? (
+                <input
+                type="text"
+                className="form-control"
+                placeholder="How do you use Napier grass on your farm? (e.g., feed for cows, selling, other purposes)"
+                name="useOfNapier"
+                value={farmerData.useOfNapier}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.useOfNapier}</span>
               )
               }</p>
             </b>
             <b>
               <h1>Cows Info</h1>
               <hr className="line"></hr>
-              <p>Number Of Cows : {editMode ? (
-                <input type="number" className="form-control" placeholder="Number Of Cows"
-                  value={farmer.numberOfCows} onChange={handleInputChange} />) : (
-                <span>{farmer1.numberOfCows}</span>
+              <p>Number Of Cows : {" "}
+                {editMode ? (
+                <input
+                type="number"
+                className="form-control"
+                placeholder="Number Of Cows"
+                name="numberOfCows"
+                value={farmerData.numberOfCows}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.numberOfCows}</span>
               )
               }</p>
-              <p>Amount of Dung Produced : {editMode ? (
-                <input type="number" className="form-control" placeholder="Dung Produced (in kg)"
-                  value={farmer.dungProduced_inKg} onChange={handleInputChange} />) : (
-                <span>{farmer1.dungProduced_inKg}</span>
+              <p>Amount of Dung Produced :{" "} {editMode ? (
+                <input
+                type="number"
+                className="form-control"
+                placeholder="Dung Produced (in kg)"
+                name="dungProduced_inKg"
+                value={farmerData.dungProduced_inKg}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.dungProduced_inKg}</span>
               )
               } kg</p>
-              <p>Amount Of Milk : {editMode ? (
-                <input type="number" className="form-control" placeholder="Amount of Milk (in Litres)"
-                  value={farmer.amountOfMilk_inLitre} onChange={handleInputChange} />) : (
-                <span>{farmer1.amountOfMilk_inLitre} Litre</span>
+              <p>Amount Of Milk :{" "} {editMode ? (
+                <input
+                type="number"
+                className="form-control"
+                placeholder="Amount of Milk (in Litres)"
+                name="amountOfMilk_inLitre"
+                value={farmerData.amountOfMilk_inLitre}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{farmerData.amountOfMilk_inLitre} Litre</span>
               )
               } </p>
             </b>
@@ -242,28 +279,40 @@ function Farmersprofile() {
               <b>
                 <h1>Others</h1>
                 <hr className="line"></hr>
-                <p>Description : {editMode ? (
-                  <input type='text' className='form-control' placeholder='description'
-                    value={farmer.description} onChange={handleInputChange}
-                  />) : (
-                  <span>{farmer1.description}</span>
+                <p>Description : {" "}{editMode ? (
+                 <input
+                 type='text'
+                 className='form-control'
+                 placeholder='description'
+                 name="description"
+                 value={farmerData.description}
+                 onChange={handleInputChange}
+               />
+             ) : (
+               <span>{farmerData.description}</span> 
                 )
                 }  </p>
-                <p>Challenges : {editMode ? (
-                  <input type='text' className='form-control' placeholder='Any specific challenges you face in your farming practices:'
-                    value={farmer.challenges} onChange={handleInputChange}
-                  />) : (
-                  <span>{farmer1.challenges}</span>
+                <p>Challenges :{" "} {editMode ? (
+                  <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Any specific challenges you face in your farming practices:'
+                  name="challenges"
+                  value={farmerData.challenges}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <span>{farmerData.challenges}</span>
                 )
                 }</p>
-                <p>interest In Training : {editMode ? (
+                <p>Interest In Training :{" "} {editMode ? (
                   <div className="radio-container">
                     <label style={{ marginTop: '15px', marginLeft: '-100px', fontSize: '18px' }}>Are you interested in any farm-related training or <br></br>assistance?</label>
                     <label>
                       <input
                         type="radio"
                         value="yes"
-                        checked={farmer.interestInTraining}
+                        checked={farmerData.interestInTraining}
                         onChange={handleRadioChange}
                       />
                       Yes
@@ -272,13 +321,13 @@ function Farmersprofile() {
                       <input
                         type="radio"
                         value="no"
-                        checked={!farmer.interestInTraining}
+                        checked={!farmerData.interestInTraining}
                         onChange={handleRadioChange}
                       />
                       No
                     </label>
                   </div>) : (
-                  <span>{farmer1.interestInTraining ? "Yes" : "No"}</span>
+                  <span>{farmerData.interestInTraining ? "Yes" : "No"}</span>
                 )
                 }</p>
               </b>
